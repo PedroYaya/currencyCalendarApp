2b79090e8453fcad6309635564ceb9e89417b8f8
Pedro <pedro.y@floraqueen.com>
2019-12-27 15:45:33 +0100

    axios logic

---
+++ src/views/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+      <v-layout :wrap="true">
+          <v-flex xs12>
+              <v-card>
+                  <v-date-picker
+                  v-model="date"
+                  full-width
+                  :min="min"
+                  :max="max"
+                  @change="getDolar(date)"
+                  >
+
+                  </v-date-picker>
+              </v-card>
+               <v-card color="error"
+                  dark>
+                    <v-card-text class="display-1 text-center">
+                      Dolar price:  {{value}}
+                    </v-card-text>
+              </v-card>
+          </v-flex>
+      </v-layout>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data(){
+    return {
+        date: new Date().toISOString().substr(0, 10),
+        min: "1984",
+        max: new Date().toISOString().substr(0, 10),
+        value: null
+    }
+  },
+  methods: {
+      async getDolar(day){
+          let arrayDate = day.split(["-"])
+          let ddmmyy = arrayDate[2]+"-"+arrayDate[1]+"-"+arrayDate[0]
+
+          try {
+            let data = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`)
+            if(data.data.serie.length > 0) {
+                this.value = await data.data.serie[0].valor
+                            // eslint-disable-next-line no-console
+                            console.log(this.value);
+            } else {
+                this.value = "No results available"
+            }
+          } catch(err) {
+            // eslint-disable-next-line no-console
+            console.log(err)
+          }
+      }
+  },
+  created () {
+    this.getDolar(this.date);
+  }
+};
+</script>
